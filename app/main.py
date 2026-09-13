070793e448225cffac83b7a442d318cad945e0d0
Saad Khalid <saad.khalid06@gmail.com>
2026-09-13 16:03:42 +0100

    feat: Add Monitor Creation Endpoint

--- app/main.py
+++ app/main.py
@@ -1,7 +1,20 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI(title = "Uptime Monitor API")
+
+class MonitorCreate(BaseModel):
+    name: str
+    url: str
 
 @app.get("/health")
 def health_check():
     return {"Status" : "OK"}
+
+@app.post("/monitors")
+def create_monitor(monitor: MonitorCreate):
+    return {
+        "ID" : 1,
+        "Name": monitor.name,
+        "URL": monitor.url
+    }
